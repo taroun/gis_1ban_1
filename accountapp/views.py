da36eb68cc0c19b7888638c829d336a57f9eb06c
taroun <aksghk4671@gmail.com>
2021-07-08 15:55:57 +0900

    7/8 1ban class commit 4

--- accountapp/views.py
+++ accountapp/views.py
@@ -10,10 +10,20 @@ from django.shortcuts import render
 #html을 가져오기
 #분기점 생성
 #render에 추가정보 보내주기 text객체에 접근 가능함
+from accountapp.models import HelloWorld
+
+
 def hello_world(request):
     if request.method == 'POST':
+
+        temp = request.POST.get('hello_world_input')
+
+        new_hello_world = HelloWorld()
+        new_hello_world.text = temp
+        new_hello_world.save()
+
         return render(request, 'accountapp/hello_world.html',
-                      context={'text': 'POST METHOD'})
+                      context={'hello_world_output': new_hello_world})
     else:
         return render(request, 'accountapp/hello_world.html',
                       context={'text': 'GET METHOD'})
