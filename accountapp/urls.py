d01cb652b2c588205ad28e066482c117cf26f79a
taroun <aksghk4671@gmail.com>
2021-07-12 16:03:58 +0900

    7/12 1ban class commit 3

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from accountapp.views import hello_world
+from accountapp.views import hello_world, AccountCreateView
 
 app_name = 'accountapp'
 
 urlpatterns = [
-    path('hello_world/', hello_world, name='hello_world')
+    path('hello_world/', hello_world, name='hello_world'),
+    #클래스에서 함수로 뱉어주는......as_view
+    path('create/',AccountCreateView.as_view(),name='create')
 ]
--- accountapp/views.py
+++ accountapp/views.py
@@ -1,3 +1,5 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
@@ -10,7 +12,8 @@ from django.shortcuts import render
 #html을 가져오기
 #분기점 생성
 #render에 추가정보 보내주기 text객체에 접근 가능함
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
+from django.views.generic import CreateView
 
 from accountapp.models import HelloWorld
 
@@ -33,3 +36,13 @@ def hello_world(request):
         hello_world_list = HelloWorld.objects.all()
         return render(request, 'accountapp/hello_world.html',
                       context={'hello_world_list': hello_world_list})
+
+
+class AccountCreateView(CreateView):
+    model = User
+    form_class = UserCreationForm
+    #클래스에서는 reverse_lazy로.. 불러오는 방식이 달라서..
+    success_url = reverse_lazy('accountapp:hello_world')
+    #회원가입 페이지
+    template_name = 'accountapp/create.html'
+    #어떤 페이지로 들어가야.라우팅..
