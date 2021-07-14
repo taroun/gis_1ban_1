67b62bb6d76fe22d1a8f17f98dc5c1eec48de6d0
taroun <aksghk4671@gmail.com>
2021-07-14 09:34:57 +0900

    7/14 1ban class commit 2

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
 from accountapp.views import hello_world, AccountCreateView
@@ -6,6 +7,9 @@ app_name = 'accountapp'
 
 urlpatterns = [
     path('hello_world/', hello_world, name='hello_world'),
+    #파라미터 지정-템플릿뭐 쓸건지
+    path('login/', LoginView.as_view(template_name='accountapp/login.html'), name = 'login'),
+    path('logout/', LogoutView.as_view(), name = 'logout'),
     #클래스에서 함수로 뱉어주는......as_view
     path('create/',AccountCreateView.as_view(),name='create')
 ]
