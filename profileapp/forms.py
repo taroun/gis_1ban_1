9e6818d20fab8a9455348b3053c161acaf9caf10
taroun <aksghk4671@gmail.com>
2021-07-26 14:53:08 +0900

    7/26 1ban class commit 1

--- profileapp/forms.py
+++ profileapp/forms.py
@@ -5,7 +5,5 @@ from profileapp.models import Profile
 
 class ProfileCreationForm(ModelForm):
     class Meta:
-        #Meta 정보-외적정보...데이터자체는 아니지만 설명해주는....
         model = Profile
-        #user는 서버내에서...나머지는 클라이언트에게 받음..
         fields = ['image', 'nickname', 'message']
