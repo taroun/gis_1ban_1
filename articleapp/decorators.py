3c3458cf0733a4d5b9d9e49f2b57fcceb7a84243
taroun <aksghk4671@gmail.com>
2021-08-04 10:13:55 +0900

    8/4 1ban class commit 3

---
+++ articleapp/decorators.py
@@ -0,0 +1,12 @@
+from articleapp.models import Article
+from django.http import HttpResponseForbidden
+
+
+def article_ownership_required(func):
+    def decorated(request, *args, **kwargs):
+        target_article = Article.objects.get(pk=kwargs['pk'])
+        if target_article.writer == request.user:
+            return func(request, *args, **kwargs)
+        else:
+            return HttpResponseForbidden()
+    return decorated
