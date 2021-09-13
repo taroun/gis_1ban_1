b13dfffcfe109d4bd99e492cc1b3d7340673b200
taroun <aksghk4671@gmail.com>
2021-09-13 09:23:40 +0900

    9/13 1ban class commit 0

--- gis_1ban_1/settings.py
+++ gis_1ban_1/settings.py
@@ -41,15 +41,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
-# Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
@@ -113,7 +105,15 @@ TEMPLATES = [
 WSGI_APPLICATION = 'gis_1ban_1.wsgi.application'
 
 
+# Database
+# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 
 # Password validation
